06d7ccb529cab19a65c1b891f289187fa0e7b4c3
Arjun Shukla <arjunlalshukla@gmail.com>
2020-03-01 14:14:25 -0700

    server error handling ready for testing

--- backpack/CS455-555-1-s20-team/p1-chatserver-filesync/src/main/scala/FileSync.scala
+++ backpack/CS455-555-1-s20-team/p1-chatserver-filesync/src/main/scala/FileSync.scala
@@ -25,13 +25,23 @@ final class Client(args: Vector[String]) {
 
 }
 
+//noinspection SpellCheckingInspection
 final class Server(args: Vector[String]) {
+  if (args.length != 1) {
+    FileSync.error("""
+      |usage:
+      |scala FileSync -client <serverhost> <localfolder>
+      |or
+      |scala FileSync -server <folder>
+      |""".stripMargin)
+  }
+
   val syncdir = new File(args(0))
   val fssdir = new File(".fss")
   val fssrc = new File(".fss/fssrc")
 
-  if (args.length != 1) {
-    FileSync.error(FileSync.usage)
+  if (!syncdir.exists || !syncdir.isDirectory) {
+    FileSync.error("folder given must exist and be a directory")
   } else if (syncdir.listFiles.exists(_.isDirectory)) {
     FileSync.error("folder given cannot contain subdirectories")
   } else if (!fssdir.exists || !fssdir.isDirectory) {
@@ -40,15 +50,55 @@ final class Server(args: Vector[String]) {
     FileSync.error("The fssrc file must exists and not be a directory")
   }
 
-  val fssrclines = Source.fromFile(fssrc.getAbsolutePath).getLines.toVector
+  private[this] val rcsrc = Source.fromFile(fssrc.getAbsolutePath)
+  val fssrclines: Seq[String] = rcsrc.getLines.toVector
     .map(_.trim).filter(_.length > 0) // for empty lines
+  rcsrc.close
 
   if (fssrclines.map(_.filter(_ == '=').length).exists(_ != 1)) {
     FileSync.error("fssrc must contain key value pairs with one equals per line")
   }
 
-  val fssrckeys = fssrclines.map { line =>
+  private[this] val fssrckeys = fssrclines.map { line =>
     val kv = line.split("=")
     kv(0).trim -> kv(0).trim
   }.toMap
+
+  val clientlist: Seq[String] = fssrckeys.get("clientlist") match {
+    case None | Some("") => Vector("localhost")
+    case Some(a) => a.split(",").map(_.trim).toVector
+  }
+
+  val interval: Int = fssrckeys.get("interval") match {
+    case None | Some("") => 60
+    case Some(a) =>
+      if (a.matches("\\d+")) {
+        a.toInt
+      } else {
+        -1
+      }
+  }
+
+  val logfile: File = fssrckeys.get("logfile") match {
+    case None | Some("") => new File(fssdir.getAbsolutePath + "/log")
+    case Some(a) => new File(a)
+  }
+
+  val timeout: Int = fssrckeys.get("timeout") match {
+    case None | Some("") => 900
+    case Some(a) =>
+      if (a.matches("\\d+")) {
+        a.toInt
+      } else {
+        -1
+      }
+  }
+
+  if (interval == -1) {
+    FileSync.error("interval must be a positive integer")
+  } else if (timeout == -1) {
+    FileSync.error("timeout must be a positive integer")
+  } else if (logfile.exists && logfile.isDirectory) {
+    FileSync.error("Log file cannot be a directory")
+  }
 }
