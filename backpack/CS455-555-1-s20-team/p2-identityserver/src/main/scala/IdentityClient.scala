ab74e8d68672e0682b9d7d151ecc377d76c0ae50
Arjun Shukla <arjunlalshukla@gmail.com>
2020-03-29 22:37:30 -0600

    cli client code ready for testing

--- backpack/CS455-555-1-s20-team/p2-identityserver/src/main/scala/IdentityClient.scala
+++ backpack/CS455-555-1-s20-team/p2-identityserver/src/main/scala/IdentityClient.scala
@@ -1,22 +1,76 @@
 import little.cli.Cli.{application, option}
 import little.cli.Implicits._
-import org.apache.commons.cli.Option
+import org.apache.commons.cli.{MissingOptionException, Option, UnrecognizedOptionException}
 
 class IdentityClient {
 
 }
 object IdentityClient {
 
-  private[this] def ob(s: String) = Option.builder(s)
+  val defaultPort = 5190
 
-  private[this] val cmd = application(
+  val defaultPassword = "changeme"
+
+  private[this] def ob(s: String): Option.Builder = Option.builder(s)
+
+  private[this] val app = application(
     "java IdClient --server <serverhost> [--numport <port#>] <query>",
     ob("s").longOpt("server").desc("the identity server to connect to")
-      .hasArg.argName("serverport>").required.build,
+      .hasArg.argName("serverport").build,
     ob("n").longOpt("numport").desc("the port to connect on")
-      .hasArg.argName("port#").build
+      .hasArg.argName("port#").`type`(Int.getClass).build,
+    ob("c").longOpt("create").desc("create a new login; usable with --password")
+      .hasArgs.numberOfArgs(2).argName("login name> <real name").build,
+    ob("l").longOpt("lookup").desc("gives all information on the given user")
+      .hasArg.argName("login name").build,
+    ob("r").longOpt("reverse-lookup").desc("look up a user by UUID")
+      .hasArg.argName("UUID").build,
+    ob("m").longOpt("modify").desc("change your login name; password protected")
+      .hasArgs.numberOfArgs(2).argName("old login name> <new login name").build,
+    ob("d").longOpt("delete").desc("delete the account; password protected")
+      .hasArg.argName("login name").build,
+    ob("g").longOpt("get").desc("retrieve information from the server")
+      .hasArg.argName("users|uids|all").build,
+    ob("h").longOpt("help").desc("print a help message").build
   )
 
-  def main(args: Array[String]): Unit = {
+  def query(args: Array[String]): Unit = {
+    val cmd = app.parse(args)
+    if (cmd.hasOption('s')) {
+      throw new IllegalArgumentException("must gives value for option 's'")
+    }
+    if (cmd.hasOptions("c", "l", "r", "m", "d", "g", "h").count(x => x) != 1) {
+      throw new IllegalArgumentException("must give exactly 1 query option")
+    }
+    val server = cmd.getOptionValue('s')
+    val port = scala.Option(cmd.getOptionValue('n')).map(_.toInt).getOrElse(defaultPort)
+    scala.Option(cmd.getOptionValue('p')) match {
+      case Some(password) =>
+        if (cmd.hasOption('c')) {
+
+        } else if (cmd.hasOption('m')) {
+
+        } else if (cmd.hasOption('d')) {
+
+        } else {
+          throw new IllegalArgumentException(
+            "option is not compatible with the password option"
+          )
+        }
+      case None =>
+        if (cmd.hasOption('l')) {
+
+        } else if (cmd.hasOption('r')) {
+
+        } else if (cmd.hasOption('g')) {
+
+        } else if (cmd.hasOption('h')) {
+
+        } else {
+          throw new IllegalArgumentException("option requires password option")
+        }
+    }
   }
+
+  def main(args: Array[String]): Unit = query(args)
 }
