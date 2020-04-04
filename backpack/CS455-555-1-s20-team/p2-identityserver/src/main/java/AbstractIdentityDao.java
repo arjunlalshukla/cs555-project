f89df8764a16bdd69eddb83aadf7aa759fd9d729
Ahmad Rezaii <ahmad.rezaii@gmail.com>
2020-04-04 09:53:26 -0600

    source formatting

--- backpack/CS455-555-1-s20-team/p2-identityserver/src/main/java/AbstractIdentityDao.java
+++ backpack/CS455-555-1-s20-team/p2-identityserver/src/main/java/AbstractIdentityDao.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.bson.types.ObjectId;
 
-public class AbstractIdentityDao {
-}
+
+public abstract class AbstractIdentityDao {
+
+        protected MongoClient mongoClient;
+        protected final String IDENTITY_DATABASE;
+        protected MongoDatabase db;
+
+        protected AbstractIdentityDao(MongoClient mongoClient, String databaseName) {
+            this.mongoClient = mongoClient;
+            IDENTITY_DATABASE = databaseName;
+            this.db = this.mongoClient.getDatabase(IDENTITY_DATABASE);
+        }
+
+        public ObjectId generateObjectID() {return new ObjectId();}
+
+    }
+
--- backpack/CS455-555-1-s20-team/p2-identityserver/src/test/java/ConnectionTest.java
+++ backpack/CS455-555-1-s20-team/p2-identityserver/src/test/java/ConnectionTest.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class ConnectionTest {
+    private static Properties properties;
+    private String mongoUri;
+    private UserDao dao;
+    private MongoClient mongoClient;
+    private String databaseName;
+
+    public static void init() throws IOException {
+        properties = new Properties();
+        properties.load(ClassLoader.getSystemResourceAsStream("application.properties"));
+    }
+
+    public static String getProperty(String propertyKey) throws IOException{
+        if (properties==null) {
+            ConnectionTest.init();
+        }
+        return properties.getProperty(propertyKey);
+    }
+
+    public MongoClient mongoClient(String connectionString){
+        ConnectionString connString = new ConnectionString(connectionString);
+
+        MongoClient mongoClient = MongoClients.create(connString);
+        return mongoClient;
+    }
+
+    @Before
+    public void setup() throws IOException {
+        mongoUri = getProperty("mongodb.uri");
+        databaseName = getProperty("mongodb.database");
+        this.mongoClient=mongoClient(mongoUri);
+        this.dao = new UserDao(mongoClient, databaseName);
+    }
+
+    @Test
+    public void testConnectionFindsDatabase() {
+
+        MongoClient mc = MongoClients.create(mongoUri);
+        boolean found = false;
+        for (String dbname : mc.listDatabaseNames()) {
+            if (databaseName.equals(dbname)) {
+                found = true;
+                break;
+            }
+        }
+        Assert.assertTrue(
+                "We can connect to MongoDB, but couldn't find expected database.",
+                found);
+    }
+
+    @Test
+    public void testUsersCount(){
+        long expected = 0;
+        Assert.assertEquals("Check the connection string", expected, dao.getUsersCount());
+    }
 }
