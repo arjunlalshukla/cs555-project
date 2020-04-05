d0851a4f6314f7949e0c80ff47ad63b488d725aa
Arjun Shukla <arjunlalshukla@gmail.com>
2020-04-04 22:35:32 -0400

    finished test code for cli

--- backpack/CS455-555-1-s20-team/p2-identityserver/src/main/scala/IdentityClient.scala
+++ backpack/CS455-555-1-s20-team/p2-identityserver/src/main/scala/IdentityClient.scala
@@ -1,74 +1,80 @@
-import little.cli.Cli.application
-import little.cli.Implicits._
-import org.apache.commons.cli.Option
+import picocli.CommandLine
 
 object IdentityClient {
 
   val defaultPort = 5190
 
-  val defaultPassword = "changeme"
-
-  private[this] def ob(s: String): Option.Builder = Option.builder(s)
-
-  private[this] val app = application(
-    "java IdClient --server <serverhost> [--numport <port#>] <query>",
-    ob("s").longOpt("server").desc("the identity server to connect to")
-      .hasArg.argName("serverport").build,
-    ob("n").longOpt("numport").desc("the port to connect on")
-      .hasArg.argName("port#").`type`(Int.getClass).build,
-    ob("c").longOpt("create").desc("create a new login; usable with --password")
-      .hasArgs.numberOfArgs(2).argName("login name> <real name").build,
-    ob("l").longOpt("lookup").desc("gives all information on the given user")
-      .hasArg.argName("login name").build,
-    ob("r").longOpt("reverse-lookup").desc("look up a user by UUID")
-      .hasArg.argName("UUID").build,
-    ob("m").longOpt("modify").desc("change your login name; password protected")
-      .hasArgs.numberOfArgs(2).argName("old login name> <new login name").build,
-    ob("d").longOpt("delete").desc("delete the account; password protected")
-      .hasArg.argName("login name").build,
-    ob("g").longOpt("get").desc("retrieve information from the server")
-      .hasArg.argName("users|uids|all").build,
-    ob("h").longOpt("help").desc("print a help message").build
-  )
-
-  def query(args: Array[String]): Unit = {
-    val cmd = app.parse(args)
-    if (cmd.hasOption('s')) {
-      throw new IllegalArgumentException("must gives value for option 's'")
-    }
-    if (cmd.hasOptions("c", "l", "r", "m", "d", "g", "h").count(x => x) != 1) {
-      throw new IllegalArgumentException("must give exactly 1 query option")
-    }
-    val server = cmd.getOptionValue('s')
-    val port = scala.Option(cmd.getOptionValue('n'))
-      .map(_.toInt).getOrElse(defaultPort)
-    scala.Option(cmd.getOptionValue('p')) match {
-      case Some(password) =>
-        if (cmd.hasOption('c')) {
-
-        } else if (cmd.hasOption('m')) {
-
-        } else if (cmd.hasOption('d')) {
-
-        } else {
-          throw new IllegalArgumentException(
-            "option is not compatible with the password option"
-          )
-        }
-      case None =>
-        if (cmd.hasOption('l')) {
-
-        } else if (cmd.hasOption('r')) {
-
-        } else if (cmd.hasOption('g')) {
-
-        } else if (cmd.hasOption('h')) {
-
-        } else {
-          throw new IllegalArgumentException("option requires password option")
-        }
-    }
+  def main(args: Array[String]): Unit = {
+    val f = IdentityClient(Array("-h"))
+    f.run()
   }
 
-  def main(args: Array[String]): Unit = query(args)
+  def apply(args: Array[String]): IdentityClient = {
+    val idc = new IdentityClient
+    val res = new CommandLine(idc).parseArgs(args: _*).errors
+    if (!res.isEmpty) {
+      throw res.get(0)
+    }
+    idc
+  }
+}
+
+final class IdentityClient extends Runnable {
+  @CommandLine.Option(names=Array("-s","--server"), required=true,
+    description=Array("the identity server to connect to"))
+  private[this] var server: String = null
+  @CommandLine.Option(names=Array("-n", "--number"),
+    description=Array("the port to connect on"))
+  private[this] var port: Int = IdentityClient.defaultPort
+  @CommandLine.Option(names=Array("-c", "--create"), arity="1..2",
+    description=Array("create a new login; usable with --password"))
+  private[this] var create: Array[String] = null
+  @CommandLine.Option(names=Array("-d", "--delete"),
+    description=Array("delete the account; password protected"))
+  private[this] var delete: String = null
+  @CommandLine.Option(names=Array("-m", "--modify"), arity="2",
+    description=Array("change your login name; password protected"))
+  private[this] var modify: Array[String] = null
+  @CommandLine.Option(names=Array("-l", "--lookup"),
+    description=Array("gives all information on the given user"))
+  private[this] var lookup: String = null
+  @CommandLine.Option(names=Array("-r", "--reverse-lookup"),
+    description=Array("look up a user by UUID"))
+  private[this] var rev_lookup: String = null
+  @CommandLine.Option(names=Array("-g", "--get"),
+    description=Array("retrieve information from the server"))
+  private[this] var get: String = null
+  @CommandLine.Option(names=Array("-p", "--password"),
+    description=Array("provide a password for certain queries"))
+  private[this] var password: String = null
+  @CommandLine.Option(names=Array("-h", "--help"), usageHelp=true)
+  private[this] var help: Boolean = false
+
+  def run(): Unit = {
+    if (help) {
+      new CommandLine(this).usage(System.out)
+      return
+    }
+    if (Seq(create,delete,modify,lookup,rev_lookup,get).count(_ != null) != 1) {
+      throw new OneQueryExcpetion
+    }
+    if (password == null) {
+      if (get != null) {
+        if (!Set("users", "uuids", "all").contains(get)) {
+          throw new GetOptionException
+        }
+      } else if (lookup != null) {}
+      else if (rev_lookup != null) {}
+      else {
+        throw new PasswordRequiredException
+      }
+    } else {
+      if (create != null) {}
+      else if (modify != null) {}
+      else if (delete != null) {}
+      else {
+        throw new PasswordIncompatibleException
+      }
+    }
+  }
 }
