b1af7dc362787dc7fe4bd2318f4a9ca519a83725
Ahmad Rezaii <ahmad.rezaii@gmail.com>
2020-05-03 13:26:13 -0600

    add prints

--- backpack/CS455-555-1-s20-team/p3-idserver-part2/src/main/scala/server/IdentityServer.scala
+++ backpack/CS455-555-1-s20-team/p3-idserver-part2/src/main/scala/server/IdentityServer.scala
@@ -111,7 +111,7 @@ final class IdentityServer(val name: String) extends IdentityServerInterface {
       .map(x => (x(0)<<24) + (x(1)<<16) + (x(2)<<8) + x(3))
       .sorted
       .map(x => s"${x>>24&0xff}.${x>>16&0xff}.${x>>8&0xff}.${x&0xff}")
-
+    println(serverList)
     val responses = serverList.takeWhile(_ != ip).takeWhile { ipAddr =>
       lazy val stub = getRegistry(ipAddr, IdentityServer.rmiPort)
         .lookup("IdentityServer").asInstanceOf[IdentityServerInterface]
